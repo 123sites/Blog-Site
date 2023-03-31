334438be3c43b7663ff0a2cec583d4cb504a0219
CF <cfreitas12345@gmail.com>
2023-03-30 18:32:28 -0700

    Cleaned up the js.

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,55 +1,60 @@
 async function signupFormH(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();  
-  const password = document.querySelector('#pw-signup').value.trim();
+  // The trim() method removes whitespace from both ends of a string and returns a new string,
+  // without modifying the original string. To return a new string with whitespace trimmed
+  // from just one end, use trimStart() or trimEnd().
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#pw-signup").value.trim();
 
   if (username && password) {
-    const response = await fetch('/api', {
-      method: 'post',
+    const response = await fetch("/api", {
+      method: "post",
       body: JSON.stringify({
         username,
-        password
+        password,
       }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
-
     // Checks response status
     if (response.ok) {
-      console.log('success');
-      alert('New user was successfully created. Ready to log in');
+      console.log("success");
+      alert("New user was successfully created. Ready to log in");
       document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
-  }  
-}
-
-async function loginFormH(event) {
-  event.preventDefault();
-
-  const username = document.querySelector('#username-login').value.trim();
-  const password = document.querySelector('#pw-login').value.trim();
-
-  if (username && password) {
-    const response = await fetch('/api/login', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-
-      // NEED TO CREATE THE DASHBOARD
-      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
   }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormH);
-document.querySelector('.login-form').addEventListener('submit', loginFormH);
+async function loginFormH(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#pw-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/login", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+
+
+      // NEED TO CREATE THE DASHBOARD
+      
+
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector(".signup-form").addEventListener("submit", signupFormH);
+document.querySelector(".login-form").addEventListener("submit", loginFormH);
