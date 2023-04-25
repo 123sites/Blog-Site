eb6509cc22756b0c482cdb150bb41e1d2c393404
CF <cfreitas12345@gmail.com>
2023-04-25 16:05:50 -0700

    Pushing to Heroku

--- controllers/signupRoutes.js
+++ controllers/signupRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-  try {
-    res.render('signup');
-  } catch (err) {
-    res.status(500).json(err);
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
   }
+  res.render('signup');
 });
+
+module.exports = router;
 
 
 // const router = require('express').Router();
 
 // router.get('/', async (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
+//   try {
+//     res.render('signup');
+//   } catch (err) {
+//     res.status(500).json(err);
 //   }
-//   res.render('signup');
 // });
-
-module.exports = router;
