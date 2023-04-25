27d9eae66c185c067c30dfbcac98adaa99e18107
CF <cfreitas12345@gmail.com>
2023-04-25 15:18:18 -0700

    Added edit-post.js and hbs.

--- controllers/signupRoutes.js
+++ controllers/signupRoutes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
+  try {
+    res.render('signup');
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render('signup');
 });
+
+
+// const router = require('express').Router();
+
+// router.get('/', async (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
+//   res.render('signup');
+// });
 
 module.exports = router;
